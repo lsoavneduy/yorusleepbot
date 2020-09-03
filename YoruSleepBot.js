23c837f6d3c0ff778f38fd973d22f7abb16deb5c
lsoavneduy <43095273+lsoavneduy@users.noreply.github.com>
2020-09-04 00:23:37 +0800

    Update YoruSleepBot.js

--- YoruSleepBot.js
+++ YoruSleepBot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const yoru = '740408209965187093';
 const token = process.env.TOKEN;
+var alarm = true;
 
 client.on('ready', () => {
     console.log('I Am Ready To Yoru Sleep :3');
@@ -10,20 +11,32 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (!msg.author.bot) {
-        if (client.users.cache.get(yoru).presence.status == 'online')
+        if (client.users.cache.get(yoru).presence.status == 'online' && alarm)
             client.channels.cache.get('750409993387507725').send("瞓啦<@" + yoru + ">~");
         else 
             console.log('Yoru is offline...');
-    }
 
-    if (msg.author.userID == yoru) {
-        msg.channel.send("瞓啦<@" + yoru + ">~ 仲傾~")
+        if (msg.author.userID == yoru && alarm) {
+            msg.channel.send("瞓啦<@" + yoru + ">~ 仲傾~")
+        }
+
+        if (msg.content == '!Let Yoru Take A Rest' && msg.author.userID == '206003875092627456') {
+            alarm = false;
+            msg.reply('咁好啦...');
+        } else if (msg.content == '!Let Yoru Take A Rest' && !msg.author.userID == '206003875092627456') {
+            msg.reply('唔要喎!');
+        } 
+
+        if (msg.content == '!Ask Yoru Sleep') {
+            alarm = true;
+            msg.reply('OJBK~');
+        }
     }
 });
 
 client.on('presenceUpdate', (oldS, newS) => {
     let yoru = '740408209965187093';
-    if (newS.userID == '740408209965187093' && newS.status == 'online')
+    if (newS.userID == '740408209965187093' && newS.status == 'online' && alarm)
         client.channels.cache.get('750409993387507725').send("<@" + yoru + ">今日早啲瞓啦~");
 });
 
